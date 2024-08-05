5c6626b247601fe8df523add693333d1f280c90a
aldieptr28 <aldiputrapratama283.com>
2024-08-05 08:48:44 +0700

    first commit

---
+++ sidebarbiasa.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>website beasiswa</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/676a7d6f76.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+  
+
+    
+
+    
+</head>
+<body>
+    
+</body>
+</html>
